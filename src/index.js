a8c35ae9ba557a49e058c8541dffff91b09a8b03
Eric <ericmetalhammer@gmail.com>
2018-09-12 14:59:29 +0300

    'add_main_and_checkup_fuctions'

--- src/index.js
+++ src/index.js
@@ -4,4 +4,45 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+var array = preferences;
+var count = 0;
+let triangles = [];
+
+array.forEach(function(value, index){
+  let check = true;
+  if(triangles.includes(value - 1)){
+    check = false;
+  }
+  if (check) {
+  let length = array.length;
+  let secondvalue;
+  let thirdvalue;
+  if(checkup(value - 1, length) ){
+    secondvalue = array[value - 1]; 
+    if(checkup(secondvalue - 1, length) ){
+      thirdvalue = array[secondvalue - 1];
+      if(secondvalue === value){
+
+        }else{
+        if(thirdvalue - 1 === index){
+          count++;
+          triangles.push(value - 1);
+          triangles.push(secondvalue - 1);
+          triangles.push(thirdvalue - 1);
+        }
+      }
+    }
+  }
+}
+});
+return count;
+ 
+};
+
+function checkup(index, length){
+  if (index >= 0 && index < length){
+    return true;
+  }else{
+    return false;
+  } 
 };
